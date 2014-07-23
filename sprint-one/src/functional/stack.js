c9b2dc05b77cf5d17ba13cbac2f43ee880894cab
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-07-23 12:22:52 -0700

    Completed stack.js under functional data structures.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function(){
+    counter && counter--;
+    var result = storage[counter];
+    delete storage[counter];
+  
+    return result;
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
