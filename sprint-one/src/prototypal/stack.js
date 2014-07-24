1b9a828822c296d7c2140a3f040b77ad843762f7
sunjieming <s.benjamin.nelson@gmail.com>
2014-07-24 15:28:40 -0700

    protypal stack passing all tests

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(stackMethods);
+  newInstance.storage = {};
+  newInstance.length = 0;
+  return newInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+  this.storage[this.length] = value;
+  this.length++;
+};
+
+stackMethods.pop = function(){
+  this.length && this.length--;
+  var result = this.storage[this.length];
+  delete this.storage[this.length];
+  return result;
+};
+
+stackMethods.size = function(){
+  return this.length;
+};
+//makeStack.prototype = stackMethods;
 
