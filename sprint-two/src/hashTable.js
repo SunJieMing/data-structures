99734570973b0227a3cc45dbc44e70519a6dfccc
sunjieming <s.benjamin.nelson@gmail.com>
2014-07-25 15:08:09 -0700

    Completed sprint 2 (No extra credit).

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,47 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  if(bucket === undefined){
+    this._storage.set(i, [k,v]);
+  }
+  else if(Array.isArray(bucket[0])){
+    bucket.push([k, v]);
+  } else {
+    this._storage.set(i, []);
+    var newArr = this._storage.get(i);
+    newArr.push(bucket, [k, v]);
+    // this._storage.set(i, [k,v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i);
+  if(bucket === null){return null;}
+  if(!Array.isArray(bucket[0])){
+    return bucket[1];
+  } else {
+    for(var j = 0; j < bucket.length; j++){
+      if(k === bucket[j][0]){
+        return bucket[j][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  if(!Array.isArray(bucket[0])){
+    this._storage.set(i, null);
+  } else {
+    for(var j = 0; j < bucket.length; j++){
+      if(k === bucket[j][0]){
+        return bucket.splice(j, 1);
+      }
+    }
+  }
 };
 
 
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,23 +2,45 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
-  return newTree;
+  //call extend on newTree and treeMethods
+  return extend(newTree, treeMethods);
 };
 
-
-
+//extend function/ extend newTree and treeMethods
+var extend = function(obj1, obj2) {
+  for (var key in obj2){
+    obj1[key] = obj2[key];
+  }
+  return obj1;
+};
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var newNode = makeTree(value);
+  if (!this.children) {
+    this.children = [newNode];
+  } else {
+    this.children.push(newNode);
+  }
+  //update children of target node (this) this.children
 };
 
 treeMethods.contains = function(target){
+  if (this.value === target){
+    return true;
+  } else if (this.children) {
+    for(var i = 0; i < this.children.length; i++){
+      if (this.children[i].contains(target)){
+        return true;
+      }
+    }
+  }
+  return false;
 
 };
 
-
+//var makeNode(value) --> { value: value, children: undefined }
 /*
  * Complexity: What is the time complexity of the above functions?
  */
