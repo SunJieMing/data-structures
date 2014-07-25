d937b78c4dfc5dab3d0ff94ae4664a9b11558530
sunjieming <s.benjamin.nelson@gmail.com>
2014-07-24 20:11:31 -0700

    completed sets.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,14 +7,28 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this[item] = item;
 };
 
 setPrototype.contains = function(item){
+  var obj = this;
+  for (var key in obj){
+    if (obj[key] === item){
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  if (this.hasOwnProperty(item)){
+    delete this[item];
+  }
+
 };
 
+
+// item cannot equal prototype methods\
 /*
  * Complexity: What is the time complexity of the above functions?
  */
