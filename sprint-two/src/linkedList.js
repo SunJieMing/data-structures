6d76ceb8eec305ef32cfa211f8414bf4631fed42
sunjieming <s.benjamin.nelson@gmail.com>
2014-07-24 19:33:20 -0700

    Finished linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,36 @@ var makeLinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  // list.counter = 1;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if(list.head === null){
+      list.tail = list.head = newNode;
+    } else {
+      list.tail.next = list.tail = newNode;
+    }
+
   };
 
   list.removeHead = function(){
+
+    var result = list.head.value;
+    list.head = list.head.next;
+    return result;
   };
 
   list.contains = function(target){
+
+    var nextHead = list.head;
+    while(nextHead.next !== null){
+      if(nextHead.value === target){
+        return true;
+      } else {
+        nextHead = nextHead.next;
+      }
+    }
+    return nextHead.value === target;
   };
 
   return list;
