dc66c1f0e60148734d23bd26a5b1377636696bf6
sunjieming <s.benjamin.nelson@gmail.com>
2014-07-25 20:10:31 -0700

    implemented binary tree class and methods.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,65 @@
 var makeBinarySearchTree = function(value){
+  var newBinaryTree = {};
+  newBinaryTree.left = null;
+  newBinaryTree.right = null;
+  newBinaryTree.value = value;
+  return extend(newBinaryTree, binaryTreeMethods);
 };
+
+var binaryTreeMethods = {};
+
+var extend = function(obj1, obj2){
+  for(var key in obj2){
+    obj1[key] = obj2[key];
+  }
+  return obj1;
+};
+
+binaryTreeMethods.insert = function(value){
+  var newNode = makeBinarySearchTree(value);
+  if (value < this.value) {
+    if (!this.left) {
+      this.left = newNode;
+    }  else {
+      this.left.insert(value);
+    }
+  } else if (value > this.value) {
+    if (!this.right) {
+      this.right = newNode;
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+binaryTreeMethods.contains = function(target){
+  if (this.value === target){
+    return true;
+  }
+  if (this.left) {
+    if (this.left.contains(target)) {
+      return true;
+    }
+  }
+  if (this.right){
+    if (this.right.contains(target)){
+      return true;
+    }
+  }
+  return false;
+};
+
+binaryTreeMethods.depthFirstLog = function(cb){
+  cb(this.value);
+  if(this.right){
+    this.right.depthFirstLog(cb);
+  }
+  if(this.left){
+    this.left.depthFirstLog(cb);
+  }
+};
+
+
 
 
 /*
